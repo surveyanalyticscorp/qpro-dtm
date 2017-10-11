2c06e4a26e0af1289bee2163e97e2f26a2dc707d
priyeshghamandi <priyesh.ghamandi@questionpro.com>
2017-10-11 18:27:50 +0530

    Fetch Data Elements Values

--- src/view/js/global.js
+++ src/view/js/global.js
@@ -1,4 +1,4 @@
-var sandbox = true;
+var sandbox = false;
 function showDataElementsList(element){
         var textBoxID = element.id.split('-')[0];
         textBox = document.getElementById(textBoxID+'-DataElement-Value');
